a38b5d317b7140226ff4d3f6a87cfc7f7009c826
xGfrost <fadelakbar528@gmail.com>
2025-02-17 12:22:42 +0700

    register users

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,19 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
-    protected $table = "users";
-    protected $PrimaryKey = "id";
-    protected $keyType = "int";
-    protected $timestamps = true;
-    protected $incrementing = true;
+    // protected $table = "users";
+    // protected $PrimaryKey = "id";
+    // protected $keyType = "int";
+    // protected $timestamps = true;
+    // protected $incrementing = true;
+
+    // protected $fillable = [
+    //     'username',
+    //     'password',
+    //     'name', 
+    // ];
+
+    protected $guarded = [];
 
     public function contacts(): HasMany{
         return $this->hasMany(Contact::class, "user_id", "id");
