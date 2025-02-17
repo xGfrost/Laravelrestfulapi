87aa3f53c6cdd2c82c7a1f95d317f2fd82ed06da
xGfrost <fadelakbar528@gmail.com>
2025-02-17 13:25:18 +0700

    Login Users API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegisterRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
@@ -9,6 +10,9 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\RequestMatcher\IsJsonRequestMatcher;
+use Illuminate\Support\Str;
+
 
 class UserController extends Controller
 {
@@ -31,5 +35,24 @@ class UserController extends Controller
         $user->save();
 
         return (new  UserResource($user))->response()->setStatusCode(201);
+    }
+
+    public function login(UserLoginRequest $request): UserResource{
+        $data = $request->validated();
+
+        $user = User::where('username', $data['username'])->first();
+        if(!$user || !Hash::check($data['password'], $user->password)){
+            throw new HttpResponseException(response([
+                "errors" => [
+                    "message" => [
+                        "username or password wrong"
+                    ]
+                ]
+            ], 400));
+        }
+        $user->token = Str::uuid()->toString();
+        $user->save();
+
+        return new UserResource($user);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/users', [UserController::class, 'register']);
+Route::post('/users/login', [UserController::class, 'login']);
